340c673dfca1f54cab81cba886db033240df54c1
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-14 13:44:37 +0900

    Update 00_sample.py

--- 00_sample.py
+++ 00_sample.py
@@ -109,14 +109,14 @@ if uploaded_file:
     choropleth = folium.Choropleth(
         geo_data=state_geo1,
         name='sigun_people',
-        data='df'+choice_selected+'00',
+        data=print('df'+choice_selected+'00'),
         columns = ['SIG_CD','지원자'],
         key_on = 'feature.properties.SIG_CD',
         nan_fill_color='black',
         fill_color='BuPu',
         fill_opactiy=0.7,
-        line_opacity=0.5
-#        legend_name='jonghab_all'
+        line_opacity=0.5,
+        legend_name='jonghab_all'
     ).add_to(m.m1) 
 
 
@@ -125,13 +125,13 @@ if uploaded_file:
                        title_cancel='Click to Exit',
                        force_separate_button=True).add_to(m.m1)
     plugins.MousePosition().add_to(m.m1)
-    plugins.MarkerCluster('regional_count'+choice_selected).add_to(m.m1)
+    plugins.MarkerCluster(print('regional_count'+choice_selected)).add_to(m.m1)
 
 
     choropleth = folium.Choropleth(
         geo_data=state_geo1,
         name='sigun_people',
-        data='df'+choice_selected+'11',
+        data=print('df'+choice_selected+'11'),
         columns = ['SIG_CD','지원자'],
         key_on = 'feature.properties.SIG_CD',
         nan_fill_color='black',
@@ -146,7 +146,7 @@ if uploaded_file:
                      title_cancel='Click to Exit',
                      force_separate_button=True).add_to(m.m2)
     plugins.MousePosition().add_to(m.m2)
-    plugins.MarkerCluster('regional_count'+choice_selected+'1').add_to(m.m2)
+    plugins.MarkerCluster(print('regional_count'+choice_selected+'1')).add_to(m.m2)
     
     m.save("듀얼맵.html", close_file=True)
     
