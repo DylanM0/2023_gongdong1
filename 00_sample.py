ba304ccd0ea96d194543f0844ed6c880cef192be
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-14 17:31:03 +0900

    Update 00_sample.py

--- 00_sample.py
+++ 00_sample.py
@@ -43,8 +43,8 @@ def generate_html_download_link(ddf):
     # towrite = StringIO()
     towrite = BytesIO(towrite.getvalue().encode())    
     # ddf.write_html(towrite, include_plotlyjs="cdn")
-    
-    ddf.save(towrite, close_file=True)
+    ddf.write_html(towrite)
+    # ddf.save(towrite, close_file=True)
 
     b64 = base64.b64encode(towrite.read()).decode()
         
