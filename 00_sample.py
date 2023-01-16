5d50cfa4b5cd9788ce3b70ca20c96b6fa8927ea0
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-16 10:01:46 +0900

    Update 00_sample.py

--- 00_sample.py
+++ 00_sample.py
@@ -192,7 +192,7 @@ if uploaded_file:
     st.subheader('Downloads:')
     generate_excel_download_link(dfa)
     
-    generate_html_download_link(html1)
+    generate_html_download_link(m)
     #generate_html_download_link(m)
 
 
